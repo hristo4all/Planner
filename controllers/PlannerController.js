51fd95eeba400eefd87b96d8da0e3fe7210c0179
Hristo Ivanov <hristo4all@abv.bg>
2021-03-09 21:41:09 +0000

    implemented mustache

--- controllers/PlannerController.js
+++ controllers/PlannerController.js
@@ -5,6 +5,88 @@ exports.landing_page = function (req, res) {
 exports.goals_list = function (req, res) {
   res.render("planner", {
     title: "Planner",
+    weeks: [
+      {
+        weekNo: 1,
+        days: [
+          {
+            day: "Monday",
+            exercise: "20 x Squats",
+            actualAchievement: "done",
+          },
+          {
+            day: "Tuesday",
+            exercise: "4 x Dumbell Press",
+            actualAchievement: "2x",
+          },
+          {
+            day: "Wednessday",
+            exercise: "4 x bicep curl",
+            actualAchievement: "3x",
+          },
+          {
+            day: "Thursday",
+            exercise: "3 x Shoulder Press",
+            actualAchievement: "done",
+          },
+          {
+            day: "Friday",
+            exercise: "4 x Leg Press",
+            actualAchievement: "2x",
+          },
+          {
+            day: "Saturday",
+            exercise: "30 x crunches",
+            actualAchievement: "done",
+          },
+          {
+            day: "Sunday",
+            exercise: "4 x Bench Press",
+            actualAchievement: "done",
+          },
+        ],
+      },
+      {
+        weekNo: 2,
+        days: [
+          {
+            day: "Monday",
+            exercise: "Fuck you",
+            actualAchievement: "done",
+          },
+          {
+            day: "Tuesday",
+            exercise: "4 x Dumbell Press",
+            actualAchievement: "2x",
+          },
+          {
+            day: "Wednessday",
+            exercise: "4 x bicep curl",
+            actualAchievement: "3x",
+          },
+          {
+            day: "Thursday",
+            exercise: "3 x Shoulder Press",
+            actualAchievement: "done",
+          },
+          {
+            day: "Friday",
+            exercise: "4 x Leg Press",
+            actualAchievement: "2x",
+          },
+          {
+            day: "Saturday",
+            exercise: "30 x crunches",
+            actualAchievement: "done",
+          },
+          {
+            day: "Sunday",
+            exercise: "4 x Bench Press",
+            actualAchievement: "done",
+          },
+        ],
+      },
+    ],
   });
 };
 
