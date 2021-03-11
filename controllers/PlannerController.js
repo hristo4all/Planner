5fd450757e4cd0b104b2374d8749bc9b0b4af3aa
Hristo Ivanov <hristo4all@abv.bg>
2021-03-11 15:11:36 +0000

    implemented neDB

--- controllers/PlannerController.js
+++ controllers/PlannerController.js
@@ -1,93 +1,21 @@
 exports.landing_page = function (req, res) {
   res.send("<h1>Welcome to my Application.</h1>");
 };
-
+const guestbookDAO = require("../models/plannerModel");
+const db = new guestbookDAO();
+db.init();
 exports.goals_list = function (req, res) {
-  res.render("planner", {
-    title: "Planner",
-    weeks: [
-      {
-        weekNo: 1,
-        days: [
-          {
-            day: "Monday",
-            exercise: "20 x Squats",
-            actualAchievement: "done",
-          },
-          {
-            day: "Tuesday",
-            exercise: "4 x Dumbell Press",
-            actualAchievement: "2x",
-          },
-          {
-            day: "Wednessday",
-            exercise: "4 x bicep curl",
-            actualAchievement: "3x",
-          },
-          {
-            day: "Thursday",
-            exercise: "3 x Shoulder Press",
-            actualAchievement: "done",
-          },
-          {
-            day: "Friday",
-            exercise: "4 x Leg Press",
-            actualAchievement: "2x",
-          },
-          {
-            day: "Saturday",
-            exercise: "30 x crunches",
-            actualAchievement: "done",
-          },
-          {
-            day: "Sunday",
-            exercise: "4 x Bench Press",
-            actualAchievement: "done",
-          },
-        ],
-      },
-      {
-        weekNo: 2,
-        days: [
-          {
-            day: "Monday",
-            exercise: "Fuck you",
-            actualAchievement: "done",
-          },
-          {
-            day: "Tuesday",
-            exercise: "4 x Dumbell Press",
-            actualAchievement: "2x",
-          },
-          {
-            day: "Wednessday",
-            exercise: "4 x bicep curl",
-            actualAchievement: "3x",
-          },
-          {
-            day: "Thursday",
-            exercise: "3 x Shoulder Press",
-            actualAchievement: "done",
-          },
-          {
-            day: "Friday",
-            exercise: "4 x Leg Press",
-            actualAchievement: "2x",
-          },
-          {
-            day: "Saturday",
-            exercise: "30 x crunches",
-            actualAchievement: "done",
-          },
-          {
-            day: "Sunday",
-            exercise: "4 x Bench Press",
-            actualAchievement: "done",
-          },
-        ],
-      },
-    ],
-  });
+  db.getAllEntries()
+    .then((list) => {
+      res.render("planner", {
+        title: "Planner",
+        weeks: list,
+      });
+      console.log("promise resolved");
+    })
+    .catch((err) => {
+      console.log("promise rejected", err);
+    });
 };
 
 /*exports.goals_list = function (req, res) {
---
+++ models/plannerModel.js
@@ -0,0 +1,118 @@
+const nedb = require("nedb");
+
+class Planner {
+  constructor(dbFilePath) {
+    if (dbFilePath) {
+      this.db = new nedb({ filename: dbFilePath, autoload: true });
+      console.log("DB connected to " + dbFilePath);
+    } else {
+      this.db = new nedb();
+    }
+  }
+  init() {
+    this.db.insert({
+      weekNo: 1,
+      days: [
+        {
+          day: "Monday",
+          exercise: "20 x Squats",
+          actualAchievement: "done",
+        },
+        {
+          day: "Tuesday",
+          exercise: "4 x Dumbell Press",
+          actualAchievement: "2x",
+        },
+        {
+          day: "Wednessday",
+          exercise: "4 x bicep curl",
+          actualAchievement: "3x",
+        },
+        {
+          day: "Thursday",
+          exercise: "3 x Shoulder Press",
+          actualAchievement: "done",
+        },
+        {
+          day: "Friday",
+          exercise: "4 x Leg Press",
+          actualAchievement: "2x",
+        },
+        {
+          day: "Saturday",
+          exercise: "30 x crunches",
+          actualAchievement: "done",
+        },
+        {
+          day: "Sunday",
+          exercise: "4 x Bench Press",
+          actualAchievement: "done",
+        },
+      ],
+    });
+    //for later debugging
+    console.log("Week 1 record inserted");
+
+    this.db.insert({
+      weekNo: 2,
+      days: [
+        {
+          day: "Monday",
+          exercise: "Fuck you",
+          actualAchievement: "done",
+        },
+        {
+          day: "Tuesday",
+          exercise: "4 x Dumbell Press",
+          actualAchievement: "2x",
+        },
+        {
+          day: "Wednessday",
+          exercise: "4 x bicep curl",
+          actualAchievement: "3x",
+        },
+        {
+          day: "Thursday",
+          exercise: "3 x Shoulder Press",
+          actualAchievement: "done",
+        },
+        {
+          day: "Friday",
+          exercise: "4 x Leg Press",
+          actualAchievement: "2x",
+        },
+        {
+          day: "Saturday",
+          exercise: "30 x crunches",
+          actualAchievement: "done",
+        },
+        {
+          day: "Sunday",
+          exercise: "4 x Bench Press",
+          actualAchievement: "done",
+        },
+      ],
+    });
+    //for later debugging
+    console.log("Week 2 record inserted");
+  }
+  getAllEntries() {
+    //return a Promise object, which can be resolved or rejected
+    return new Promise((resolve, reject) => {
+      //use the find() function of the database to get the data,
+      //error first callback function, err for error, entries for data
+      this.db.find({}, function (err, entries) {
+        //if error occurs reject Promise
+        if (err) {
+          reject(err);
+          //if no error resolve the promise & return the data
+        } else {
+          resolve(entries);
+          //to see what the returned data looks like
+          console.log("function all() returns: ", entries);
+        }
+      });
+    });
+  }
+}
+module.exports = Planner;
